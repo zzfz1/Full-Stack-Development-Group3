00a33cb40b8f30740f7b81878daf589effa63859
Jasmin Akter Chowdhury <jasmine.chy12223@gmail.com>
2023-04-08 17:29:40 +0200

    added react-icons package

--- client/src/components/footer/index.jsx
+++ client/src/components/footer/index.jsx
@@ -140,3 +140,75 @@ export default function LargeWithNewsletter() {
     </Box>
   );
 }
+
+export default function LargeWithNewsletter() {
+  return (
+    <Box
+      bg={useColorModeValue('gray.50', 'gray.900')}
+      color={useColorModeValue('gray.700', 'gray.200')}>
+      <Container as={Stack} maxW={'6xl'} py={10}>
+        <SimpleGrid
+          templateColumns={{ sm: '1fr 1fr', md: '2fr 1fr 1fr 2fr' }}
+          spacing={8}>
+          <Stack spacing={6}>
+            <Box>
+              <Logo color={useColorModeValue('gray.700', 'white')} />
+            </Box>
+            <Text fontSize={'sm'}>
+              © 2022 Chakra Templates. All rights reserved
+            </Text>
+            <Stack direction={'row'} spacing={6}>
+              <SocialButton label={'Twitter'} href={'#'}>
+                {/* <FaTwitter /> */}
+              </SocialButton>
+              <SocialButton label={'YouTube'} href={'#'}>
+                {/* <FaYoutube /> */}
+              </SocialButton>
+              <SocialButton label={'Instagram'} href={'#'}>
+                {/* <FaInstagram /> */}
+              </SocialButton>
+            </Stack>
+          </Stack>
+          <Stack align={'flex-start'}>
+            <ListHeader>Company</ListHeader>
+            <Link href={'#'}>About us</Link>
+            <Link href={'#'}>Blog</Link>
+            <Link href={'#'}>Contact us</Link>
+            <Link href={'#'}>Pricing</Link>
+            <Link href={'#'}>Testimonials</Link>
+          </Stack>
+          <Stack align={'flex-start'}>
+            <ListHeader>Support</ListHeader>
+            <Link href={'#'}>Help Center</Link>
+            <Link href={'#'}>Terms of Service</Link>
+            <Link href={'#'}>Legal</Link>
+            <Link href={'#'}>Privacy Policy</Link>
+            <Link href={'#'}>Satus</Link>
+          </Stack>
+          <Stack align={'flex-start'}>
+            <ListHeader>Stay up to date</ListHeader>
+            <Stack direction={'row'}>
+              <Input
+                placeholder={'Your email address'}
+                bg={useColorModeValue('blackAlpha.100', 'whiteAlpha.100')}
+                border={0}
+                _focus={{
+                  bg: 'whiteAlpha.300',
+                }}
+              />
+              <IconButton
+                bg={useColorModeValue('green.400', 'green.800')}
+                color={useColorModeValue('white', 'gray.800')}
+                _hover={{
+                  bg: 'green.600',
+                }}
+                aria-label="Subscribe"
+                // icon={<BiMailSend />}
+              />
+            </Stack>
+          </Stack>
+        </SimpleGrid>
+      </Container>
+    </Box>
+  );
+}
