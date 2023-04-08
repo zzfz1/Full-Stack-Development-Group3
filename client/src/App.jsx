8a594c118a50b97105e70eada9185796686a0d22
Jasmin Akter Chowdhury <jasmine.chy12223@gmail.com>
2023-04-08 17:45:05 +0200

    changing bg-color

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,10 @@
 import "./App.css";
 import { Button, ButtonGroup } from "@chakra-ui/react";
 import LargeWithNewsletter from "./components/footer";
+import Home from "./pages/Home";
 
 function App() {
-  return <LargeWithNewsletter/>;
+  return <Home />;
 }
 
 export default App;
