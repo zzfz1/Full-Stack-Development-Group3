3cddf0f9ffe73ca7ae2530e20f853fc6d9238ef6
Konstantin-Filipov <kosio1234545@gmail.com>
2023-04-08 16:15:46 +0200

    test button

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import "./App.css";
 import { Button, ButtonGroup } from "@chakra-ui/react";
 
 function App() {
-  return <Button colorScheme="blue">Button</Button>;
+  return <Button colorScheme="blue">Button here</Button>;
 }
 
 export default App;
