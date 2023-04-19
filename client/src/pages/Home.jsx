df3d30f4268dd56d2ca395dc216334d4e8c8723f
Murwan Eisa <eisa.murwan@gmail.com>
2023-04-19 19:55:53 +0200

    finihsin the restpassword fields and validation

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,26 +2,14 @@ import React from "react";
 import Footer from "../components/footer/index";
 import Hero from "../components/hero";
 import { Text } from "@chakra-ui/react";
+import ResetPassword from "../components/resetPassword";
 
 const Home = () => {
   return (
     <div>
-      <Hero />
-      <Text m="20">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
-        mollitia, molestiae quas vel sint commodi repudiandae consequuntur
-        voluptatum laborum numquam blanditiis harum quisquam eius sed odit
-        fugiat iusto fuga praesentium optio, eaque rerum! Provident similique
-        accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut
-        molestias architecto voluptate aliquam nihil, eveniet aliquid culpa
-        officia aut! Impedit sit sunt quaerat, odit, tenetur error, harum
-        nesciunt ipsum debitis quas aliquid. Reprehenderit, quia. Quo neque
-        error repudiandae fuga? Ipsa laudantium molestias eos sapiente officiis
-        modi at sunt excepturi expedita sint? Sed quibusdam recusandae alias
-        error harum maxime adipisci amet laborum. Perspiciatis minima nesciunt
-        dolorem! Officiis iure rerum voluptates a cumque velit{" "}
-      </Text>
-      <Footer />
+      {/* <Hero /> */}
+      <ResetPassword />
+      {/* <Footer /> */}
     </div>
   );
 };
