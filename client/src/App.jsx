9222bad677bab5a740b94e487af6684f0f181d3f
Junxin Zheng - Gin <junxin.zheng0001@stud.hkr.se>
2023-04-19 23:43:43 +0200

    Basic Route setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,43 @@
 import "./App.css";
-import { Button, ButtonGroup } from "@chakra-ui/react";
+import Login from "./components/Login";
+import Footer from "./components/footer";
 import Home from "./pages/Home";
+import { Outlet } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <>
+        {/* <Header /> */}
+        <Outlet />
+        <Footer />
+      </>
+    ),
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      // {
+      //   path: "/register",
+      //   element: <Register />,
+      // },
+      // {
+      //   path: "/reset",
+      //   element: <Reset />,
+      // },
+    ],
+  },
+]);
 
 function App() {
-  return <Home />;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
