4ff4ae014922ba92fc544731d18d68219920c1c3
Konstantin-Filipov <kosio1234545@gmail.com>
2023-04-25 16:05:59 +0200

    navbar version update

--- client/src/pages/Checkout.jsx
+++ client/src/pages/Checkout.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Header from "../components/header/navbar2";
+import Header from "../components/header/navbar";
 
 const Checkout = () => {
   return (
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Header from "../components/header/navbar2";
+import Header from "../components/header/navbar";
 import Footer from "../components/footer/index";
 import Hero from "../components/hero";
 const Home = () => {
