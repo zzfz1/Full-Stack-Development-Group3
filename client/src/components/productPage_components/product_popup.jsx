84525632f288a07d3185894ab5023ea74cd588e8
Konstantin-Filipov <kosio1234545@gmail.com>
2023-05-07 10:39:34 +0200

    image slider functionality for popup product

--- client/src/components/productPage_components/product_popup.jsx
+++ client/src/components/productPage_components/product_popup.jsx
@@ -1,44 +1,55 @@
-import { 
-    Button,
-    IconButton,
-    Modal,
-    ModalHeader,
-    ModalBody, 
-    ModalFooter, 
-    ModalOverlay, 
-    ModalContent, 
-    ModalCloseButton,
-    Stack,
-    Text
-} from '@chakra-ui/react'
-import { ArrowBackIcon, ArrowForwardIcon } from '@chakra-ui/icons'
+import 
+{ 
+  Box,
+  Button,
+  ButtonGroup,
+  Flex,
+  IconButton,
+  Modal,
+  ModalHeader,
+  ModalBody, 
+  ModalFooter, 
+  ModalOverlay, 
+  ModalContent, 
+  ModalCloseButton,
+  Stack,
+  Text
+} from '@chakra-ui/react';
+import Slider from './product_popup_slider';
 
 function ProductCard({item, isOpen, onClose})
 {
+  const images = item.images;
+
   return (
   <Modal isOpen={isOpen} onClose={onClose}>
+    
     <ModalOverlay />
-    <ModalContent>
-      <ModalHeader>{item.brand} - {item.model}</ModalHeader>
-      <ModalCloseButton />
-      <ModalBody>
-          <div >
-            <IconButton aria-label='slide left' icon={<ArrowBackIcon />} />
-            <img src={item.img}/>
-            <IconButton aria-label='slide right' icon={<ArrowForwardIcon />} />
-          </div>
-        <Stack>
-          <Text>{item.description}</Text>
-        </Stack>
-      </ModalBody>
 
-      <ModalFooter>
-        <Button colorScheme='blue' mr={3} onClick={onClose}>
-          Close
-        </Button>
-        <Button variant='ghost'>Add Cart</Button>
-      </ModalFooter>
+      <ModalContent>
+
+        <ModalHeader>{item.brand} - {item.model}</ModalHeader>
+        <ModalCloseButton />
+        
+        <ModalBody>
+
+          <Slider images = {images}/>
+
+          <Text>{item.description}</Text>
+
+        </ModalBody>
+
+        <ModalFooter>
+
+          <Button colorScheme='blue' mr={3} onClick={onClose}>
+            Close
+          </Button>
+          <Button variant='ghost'>Add Cart</Button>
+        
+        </ModalFooter>
+  
     </ModalContent>
+  
   </Modal>
   )
 }
