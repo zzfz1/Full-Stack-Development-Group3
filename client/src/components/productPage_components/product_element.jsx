3f515e4d8c2d6aed831458e85025488875375140
Murwan Eisa <eisa.murwan@gmail.com>
2023-05-08 20:30:09 +0200

    fix the button layout

--- client/src/components/productPage_components/product_element.jsx
+++ client/src/components/productPage_components/product_element.jsx
@@ -1,13 +1,25 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import ProductCard from "./product_popup";
-import { AspectRatio, Card, CardBody, CardFooter, Image, Divider, Button, ButtonGroup, Text, Stack, Heading, Flex, Box } from '@chakra-ui/react'
-  
-function ProductElement ({item})
-{
+import {
+  AspectRatio,
+  Card,
+  CardBody,
+  CardFooter,
+  Image,
+  Divider,
+  Button,
+  ButtonGroup,
+  Text,
+  Stack,
+  Heading,
+  Flex,
+  Box,
+} from "@chakra-ui/react";
+
+function ProductElement({ item }) {
   const [openProductCard, setOpenProductCard] = useState(null);
-  
-  const handleOpenProductCard = (id) =>
-  {
+
+  const handleOpenProductCard = (id) => {
     setOpenProductCard(id);
   };
 
@@ -15,47 +27,59 @@ function ProductElement ({item})
     setOpenProductCard(null);
   };
 
-    return (
-      <Card>
+  return (
+    <Card>
+      <CardBody>
+        <AspectRatio maxW="100%" ratio={1}>
+          <Image src={item.images[0]} borderRadius="lg" />
+        </AspectRatio>
 
-        <CardBody>
+        <Stack mt="6" spacing="2">
+          <Heading size="sm">
+            {item.brand} - {item.model}
+          </Heading>
+          <Text color="blue.600" fontSize="sm">
+            {item.price}$
+          </Text>
+        </Stack>
+      </CardBody>
 
-          <AspectRatio maxW='100%' ratio={1}>
-            <Image
-              src={item.images[0]}
-              borderRadius='lg'
-            />
-          </AspectRatio>
+      <Divider />
 
-          <Stack mt='6' spacing='2'>
-            <Heading size='sm'>{item.brand} - {item.model}</Heading>
-            <Text color='blue.600' fontSize='sm'>
-              {item.price}$
-            </Text>
+      <CardFooter align="center" justify="center">
+        <ButtonGroup spacing="2">
+          <Stack
+            direction={{
+              base: "column",
+              sm: "column",
+              md: "row",
+              lg: "row",
+              xl: "row",
+            }}
+            align="center"
+            justify="center"
+          >
+            <Button
+              variant="outline"
+              colorScheme="blue"
+              onClick={() => handleOpenProductCard(item.id)}
+            >
+              View
+            </Button>
+            {openProductCard === item.id && (
+              <ProductCard
+                item={item}
+                isOpen={true}
+                onClose={handleCloseProductCard}
+              />
+            )}
+            <Button variant="solid" color="white" bg="#38A169">
+              Add to cart
+            </Button>
           </Stack>
-
-        </CardBody>
-
-        <Divider />
-        
-        <CardFooter>
-
-          <ButtonGroup spacing='2'>
-            <Stack direction= {{base : "column", md : "row"}}  align = "center" justify="center">
-              <Button  variant='solid' colorScheme='blue' onClick={() => handleOpenProductCard(item.id)}>
-                View
-              </Button>
-              {openProductCard === item.id && (<ProductCard item={item} isOpen={true} onClose={handleCloseProductCard} />)}
-              <Button  variant='ghost' colorScheme='blue'>
-                Add to cart
-              </Button>
-            </Stack>
-          </ButtonGroup>
-          
-        </CardFooter>
-
-      </Card>
-    )
-  }
+        </ButtonGroup>
+      </CardFooter>
+    </Card>
+  );
+}
 export default ProductElement;
-  
