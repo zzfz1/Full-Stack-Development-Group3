1e32bfc31c0a8221f47a2e9a6908ba24135fe83b
SebastianMuenz1988 <sebastian.muenz.24@gmail.com>
2023-05-12 17:18:22 +0200

    CategoryController

--- server/controllers/categoryController.js
+++ server/controllers/categoryController.js
@@ -1,119 +1,160 @@
-import Category from "../models/category.js";
-import CategoryProperty from "../models/categoryProperty.js";
+import Product from "../models/product.js";
 import slugify from "slugify";
 
-class CategoryController {
-  async createCategory(req, res) {
+class ProductController {
+  async createProduct(req, res) {
     try {
-      const { name, categoryProperties } = req.body;
-      const category = new Category({ name, categoryProperties });
-      await category.save();
-      res.status(201).json(category);
+      const { name, image, brand, category, description, properties, rating, numReviews, price, countInStock } = req.body;
+
+      const product = new Product({
+        name,
+        image,
+        brand,
+        category,
+        description,
+        properties,
+        rating,
+        numReviews,
+        price,
+        countInStock,
+      });
+
+      await product.save();
+      res.status(201).json(product);
     } catch (error) {
-      res.status(500).json({ message: "Internal server error", error });
+      res.status(500).json({ message: "Internal server error", error: error.message });
     }
   }
 
-  async getAllCategories(req, res) {
+  async getAllProducts(req, res) {
     try {
-      const categories = await Category.find({}).populate("categoryProperties");
-      res.json(categories);
+      const products = await Product.find({}).populate([
+        {
+          path: "category",
+          select: "name slug",
+          options: { lean: true },
+        },
+        {
+          path: "properties.categoryProperty",
+          select: "key",
+          options: { lean: true },
+        },
+      ]);
+
+      res.json(products);
     } catch (error) {
+      console.log(error.message);
       res.status(500).json({ message: "Internal server error" });
     }
   }
 
-  async getCategoryBySlug(req, res) {
+  async getProductBySlug(req, res) {
     try {
-      const category = await Category.findOne({ slug: req.params.slug }).populate("categoryProperties");
+      const product = await Product.findOne({ slug: req.params.slug }).populate([
+        {
+          path: "category",
+          select: "name slug",
+          options: { lean: true },
+        },
+        {
+          path: "properties.categoryProperty",
+          select: "key",
+          options: { lean: true },
+        },
+      ]);
 
-      if (!category) {
-        return res.status(404).json({ message: "Category not found" });
+      if (!product) {
+        return res.status(404).json({ message: "Product not found" });
       }
 
-      res.status(200).json(category);
+      res.status(200).json(product);
     } catch (error) {
       res.status(500).json({ message: "Internal server error" });
     }
   }
 
-  async updateCategory(req, res) {
+  async updateProduct(req, res) {
     try {
-      const oldslug = req.params.slug;
-      const { name, categoryProperties } = req.body;
-      const category = await Category.findOne({ slug: oldslug });
+      const { name, image, brand, category, description, properties, rating, numReviews, price, countInStock } = req.body;
 
-      if (!category) {
-        return res.status(404).json({ message: "Category not found" });
+      const product = await Product.findOne({ slug: req.params.slug });
+
+      if (!product) {
+        return res.status(404).json({ message: "Product not found" });
       }
 
-      category.name = name;
-      category.categoryProperties = categoryProperties;
-      category.slug = slugify(name, { lower: true, strict: true });
-      await category.save();
-      res.status(200).json(category);
-    } catch (error) {
-      res.status(500).json({ message: "Internal server error" });
-    }
-  }
+      const nameChanged = product.name !== name;
 
-  async deleteCategory(req, res) {
-    try {
-      const slug = req.params.slug;
-      const category = await Category.findOne({ slug });
+      product.name = name;
+      product.image = image;
+      product.brand = brand;
+      product.category = category;
+      product.description = description;
+      product.properties = properties;
+      product.rating = rating;
+      product.numReviews = numReviews;
+      product.price = price;
+      product.countInStock = countInStock;
 
-      if (!category) {
-        return res.status(404).json({ message: "Category not found" });
+      if (nameChanged) {
+        product.slug = slugify(name, { lower: true, strict: true });
       }
 
-      await category.remove();
-      res.status(200).json({ message: "Category removed" });
+      await product.save();
+      res.status(200).json(product);
     } catch (error) {
+      console.log(error);
       res.status(500).json({ message: "Internal server error" });
     }
   }
 
-  async deleteProperty(req, res) {
+  async deleteProduct(req, res) {
     try {
-      const id = req.params.id;
-      const property = await CategoryProperty.findById(id);
+      const product = await Product.findOne({ slug: req.params.slug });
 
-      if (!property) {
-        return res.status(404).json({ message: "Property not found" });
+      if (!product) {
+        return res.status(404).json({ message: "Product not found" });
       }
 
-      await property.remove();
-      res.status(200).json({ message: "Property removed" });
+      await product.remove();
+      res.status(200).json({ message: "Product removed" });
     } catch (error) {
       res.status(500).json({ message: "Internal server error" });
     }
   }
 
-  async getCategoryProperties(req, res) {
+  async createProductReview(req, res) {
     try {
-      const categories = await CategoryProperty.find({});
-      res.json(categories);
-    } catch (error) {
-      res.status(500).json({ message: "Internal server error" });
-    }
-  }
+      const { rating, comment } = req.body;
+      const product = await Product.findOne({ slug: req.params.slug });
 
-  async createProperty(req, res) {
-    try {
-      const { key } = req.body;
-      const existingCategoryProperty = await CategoryProperty.findOne({ key });
-
-      if (existingCategoryProperty) {
-        return res.status(400).json({ message: "Category Property already exists!" });
+      if (!product) {
+        return res.status(404).json({ message: "Product not found" });
       }
 
-      const categoryProperty = new CategoryProperty({ key });
-      await categoryProperty.save();
-      res.status(201).json(categoryProperty);
+      const alreadyReviewed = product.reviews.find((review) => review.user.toString() === req.user.id.toString());
+
+      if (alreadyReviewed) {
+        return res.status(400).json({ message: "You have already reviewed this product" });
+      }
+
+      const review = {
+        user: req.user.id,
+        name: req.user.username,
+        rating: Number(rating),
+        comment,
+      };
+
+      product.reviews.push(review);
+      product.numReviews = product.reviews.length;
+      product.rating = product.reviews.reduce((acc, curr) => curr.rating + acc, 0) / product.reviews.length;
+
+      await product.save();
+      res.status(201).json({ message: "Review added" });
     } catch (error) {
-      res.status(500).json({ message: "Internal server error", error });
+      res.status(500).json({ message: "Internal server error" });
     }
   }
 }
 
-export default CategoryController;
+export default ProductController;
