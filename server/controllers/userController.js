6198ed2d229f9afabf185f9b8700a123efa459db
SebastianMuenz1988 <sebastian.muenz.24@gmail.com>
2023-05-14 18:25:02 +0200

    userController update

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -216,6 +216,7 @@ class UserController {
       res.status(500).json({ message: "Internal server error", error });
     }
   }
+
   async updateOrAddShippingAddress(req, res) {
     try {
       const user = await User.findById(req.params.id); // ID of the user
@@ -271,8 +272,14 @@ class UserController {
 
   async deleteUser(req, res) {
     try {
-      await User.findByIdAndDelete(req.params.id);
-      res.status(200).json({ message: "User has been deleted" });
+      const user = await User.findOne({ slug: req.params.slug });
+
+      if (user) {
+        await User.findByIdAndDelete(user._id);
+        res.status(200).json({ message: "User has been deleted" });
+      } else {
+        res.status(404).json({ message: "User not found" });
+      }
     } catch (err) {
       res.status(500).json({ message: "Internal server error" });
     }
