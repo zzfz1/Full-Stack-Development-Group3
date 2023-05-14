61f38f804b936b394ab30208a9fcf90e4a2981a4
Konstantin-Filipov <kosio1234545@gmail.com>
2023-05-14 11:52:28 +0200

    delete gift wrapping

--- client/src/components/shopCart_components/cart-productMeta.jsx
+++ client/src/components/shopCart_components/cart-productMeta.jsx
@@ -33,10 +33,8 @@ import {
           </Stack>
           {isGiftWrapping && (
             <HStack spacing="1" mt="3" color={mode('gray.600', 'gray.400')}>
-              <Icon as={FiGift} boxSize="4" />
-              <Link fontSize="sm" textDecoration="underline">
-                Add gift wrapping
-              </Link>
+              
+              
             </HStack>
           )}
         </Box>
