9a22c731a81d4ac8092a9dbe3f8664c7ba69fc31
Murwan Eisa <eisa.murwan@gmail.com>
2023-05-15 23:47:44 +0200

    finished the chectout

--- client/src/components/shopCart_components/cart-orderSummary.jsx
+++ client/src/components/shopCart_components/cart-orderSummary.jsx
@@ -1,69 +1,81 @@
 import {
-    Button,
-    Flex,
-    Heading,
-    Link,
-    Stack,
-    Text,
-    useColorModeValue as mode,
-  } from '@chakra-ui/react'
-  import { FaArrowRight } from 'react-icons/fa'
-  import { formatPrice } from './cart-priceTag'
-import { useDispatch } from 'react-redux'
-import { clearArray } from '../../redux/cartRedux'
-import { toast } from 'react-toastify'
-  const OrderSummaryItem = (props) => {
-    const { label, value, children } = props
-    return (
-      <Flex justify="space-between" fontSize="sm">
-        <Text fontWeight="medium" color={mode('gray.600', 'gray.400')}>
-          {label}
-        </Text>
-        {value ? <Text fontWeight="medium">{value}</Text> : children}
-      </Flex>
-    )
-  }
-  
-  export const CartOrderSummary = (props) => {
-    const {total} = props; 
-    const dispatch = useDispatch();
+  Button,
+  Flex,
+  Heading,
+  Link,
+  Stack,
+  Text,
+  useColorModeValue as mode,
+} from "@chakra-ui/react";
+import { FaArrowRight } from "react-icons/fa";
+import { formatPrice } from "./cart-priceTag";
+import { useDispatch, useSelector } from "react-redux";
+import { clearArray } from "../../redux/cartRedux";
+import { toast } from "react-toastify";
+const OrderSummaryItem = (props) => {
+  const { label, value, children } = props;
+  return (
+    <Flex justify="space-between" fontSize="sm">
+      <Text fontWeight="medium" color={mode("gray.600", "gray.400")}>
+        {label}
+      </Text>
+      {value ? <Text fontWeight="medium">{value}</Text> : children}
+    </Flex>
+  );
+};
 
-    const handleCheckout = () => 
-    {
+export const CartOrderSummary = (props) => {
+  const { total } = props;
+  const user = useSelector((state) => state.user.currentUser);
+  const dispatch = useDispatch();
+
+  const handleCheckout = () => {
+    if (user) {
       dispatch(clearArray());
       toast.success("Thank your for the purchase", {
         position: toast.POSITION.TOP_CENTER,
       });
+    } else {
+      toast.error("Oop! you need to login", {
+        position: toast.POSITION.TOP_CENTER,
+      });
     }
+  };
 
-    return (
-      <Stack spacing="8" borderWidth="1px" rounded="lg" padding="8" width="full">
-        <Heading size="md">Order Summary</Heading>
-  
-        <Stack spacing="6">
-          <OrderSummaryItem label="Subtotal" value={formatPrice(total)} />
-          <OrderSummaryItem label="Shipping + Tax">
-            <Link href="#" textDecor="underline">
-              Calculate shipping
-            </Link>
-          </OrderSummaryItem>
-          <OrderSummaryItem label="Coupon Code">
-            <Link href="#" textDecor="underline">
-              Add coupon code
-            </Link>
-          </OrderSummaryItem>
-          <Flex justify="space-between">
-            <Text fontSize="lg" fontWeight="semibold">
-              Total
-            </Text>
-            <Text fontSize="xl" fontWeight="extrabold">
-              {formatPrice(total)}
-            </Text>
-          </Flex>
-        </Stack>
-        <Button colorScheme="blue" size="lg" fontSize="md" rightIcon={<FaArrowRight />} onClick={handleCheckout}>
-          Checkout
-        </Button>
+  return (
+    <Stack spacing="8" borderWidth="1px" rounded="lg" padding="8" width="full">
+      <Heading size="md">Order Summary</Heading>
+
+      <Stack spacing="6">
+        <OrderSummaryItem label="Subtotal" value={formatPrice(total)} />
+        <OrderSummaryItem label="Shipping + Tax">
+          <Link href="#" textDecor="underline">
+            Calculate shipping
+          </Link>
+        </OrderSummaryItem>
+        <OrderSummaryItem label="Coupon Code">
+          <Link href="#" textDecor="underline">
+            Add coupon code
+          </Link>
+        </OrderSummaryItem>
+        <Flex justify="space-between">
+          <Text fontSize="lg" fontWeight="semibold">
+            Total
+          </Text>
+          <Text fontSize="xl" fontWeight="extrabold">
+            {formatPrice(total)}
+          </Text>
+        </Flex>
       </Stack>
-    )
-  }
+      <Button
+        colorScheme="blue"
+        size="lg"
+        fontSize="md"
+        rightIcon={<FaArrowRight />}
+        onClick={handleCheckout}
+      >
+        Checkout
+      </Button>
+    </Stack>
+  );
+};
